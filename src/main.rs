b9e153a15bcc09ea4f7227c2fbbfd6229e42f294
Rolf van der Tang <vdt.rolf@xs4all.nl>
2025-07-04 07:53:57 +0200

    Second commit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 fn main() {
-    println!("Hello, world!");
-
      // Static (referenced to)
     static PI_VALUE: f32 = 3.14162758;
 
@@ -9,12 +7,17 @@ fn main() {
     const ARRAY_POS:usize = 1;
 
     // Variables
-    let mut x:u8 = 11;
+    let mut x:u8 = 11; // mut for mutable
     let y:u8 = 12; 
     let _z:bool = true; // not used _
     x += y;
     let a_char: char = 'f';
-    
+    let a_string = "A fixed string"; // immutable string
+    let mut a_mut_string = String::from("tes"); // muttable string
+    a_mut_string.push('t');
+
+    println!("Hello, world with {a_string}! and a mutable: {a_mut_string}");
+
     // Arrays
     let tst_array: [u8; 3 ] = [1,2,4];
     let tst_array2 = [0;3];
