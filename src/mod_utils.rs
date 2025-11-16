223d019833709a5f917ea0c4c09f3b1d0c349963
Rolf van der Tang <96309486+vdtrolf@users.noreply.github.com>
2025-11-16 08:52:00 +0100

    Refactor print_md to print_md_txt and add print_line

--- src/mod_utils.rs
+++ src/mod_utils.rs
@@ -1,41 +1,48 @@
 use colored::Colorize;
-pub fn print_md(txt_md: [&str; 16]) {
-    for l in txt_md {
-        if l.starts_with("###") {
-            println!("{}", l[3..].trim().on_red().bold());
-        } else if l.starts_with("##") {
-            println!("{}", l[2..].trim().yellow().bold());
-        } else if l.starts_with("#") {
-            println!("{}", l[1..].trim().green().bold());
-        } else {
-            let parts = l.split(" ");
-            let mut inb: bool = false;
-            let mut buf: String = ("").to_string();
-            for t in parts {
-                if t.starts_with("**") && t.ends_with("**") {
-                    let mut s = t[2..].to_string();
-                    s.pop();
-                    s.pop();
-                    print!("{} ", s.cyan().bold());
-                } else if t.starts_with("**") {
-                    inb = true;
-                    buf = (&t[2..]).to_string();
-                } else if t.ends_with("**") && inb {
-                    let mut newbuf = buf.to_string() + " " + t;
-                    newbuf.pop();
-                    newbuf.pop();
-                    print!("{} ", newbuf.cyan().bold());
-                    buf = ("").to_string();
-                    inb = false;
-                } else if inb {
-                    let newbuf = buf.to_string() + " " + t;
-                    buf = newbuf;
-                } else {
-                    print!("{} ", t);
-                }
+
+pub fn print_md_txt(txt_md: &str) {
+    let parts = txt_md.split("\n");
+    for l in parts {
+        print_line(l);
+    }
+    println!("");
+}
+
+fn print_line(l: &str) {
+    if l.starts_with("###") {
+        println!("{}", l[3..].trim().on_red().bold());
+    } else if l.starts_with("##") {
+        println!("{}", l[2..].trim().yellow().bold());
+    } else if l.starts_with("#") {
+        println!("{}", l[1..].trim().green().bold());
+    } else {
+        let parts = l.split(" ");
+        let mut inb: bool = false;
+        let mut buf: String = ("").to_string();
+        for t in parts {
+            if t.starts_with("**") && t.ends_with("**") {
+                let mut s = t[2..].to_string();
+                s.pop();
+                s.pop();
+                print!("{} ", s.cyan().bold());
+            } else if t.starts_with("**") {
+                inb = true;
+                buf = (&t[2..]).to_string();
+            } else if t.ends_with("**") && inb {
+                let mut newbuf = buf.to_string() + " " + t;
+                newbuf.pop();
+                newbuf.pop();
+                print!("{} ", newbuf.cyan().bold());
+                buf = ("").to_string();
+                inb = false;
+            } else if inb {
+                let newbuf = buf.to_string() + " " + t;
+                buf = newbuf;
+            } else {
+                print!("{} ", t);
             }
         }
-        println!("");
+        print!("\n");
     }
 }
 
