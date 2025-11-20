c854cb573e75866b166186b926929f1abe3daa97
vdtrolf <vdt.rolf@xs4all.nl>
2025-11-20 13:05:53 +0100

    Change in 9

--- src/mod_09_struct.rs
+++ src/mod_09_struct.rs
@@ -4,7 +4,7 @@ use rust_learn::mod_utils::print_title;
 static TITLE: &str = " 9-Struct";
 static EXP_TEXT: &str = "**Classical structs** :
 > struct Astruct { f1 : String, f2 : u8}
-> let (mut) A = Astruct { f1 : \"tata\".to_string(), f2 : 17 };
+> let ^(mut)^ A = Astruct { f1 : \"tata\".to_string(), f2 : 17 };
 > println!(\"{}\",A.f2); ^( 17 )^
 > A.2 = 21; ^( possible if A is mutable )^
 
